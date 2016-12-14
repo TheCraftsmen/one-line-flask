ca3cc0da7b8fec960ff8a4e4f546ee7743ec1abf
TheCraftsmen <castro.blas.martin@gmail.com>
2016-12-14 00:40:11 -0300

    reduccion de codigo

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
 from flask.views import MethodView as MV
-
-
-get = lambda self: "devuelve todo"
-post = lambda self: "crear"
-put = lambda self: "actualizar"
-
-map(lambda x: [x.add_url_rule('/', 'index', lambda: 'hola'), x.add_url_rule('/cust',view_func=type('Cust',(MV,),{row: eval('%s' % row) for row in ['get','post','put']}).as_view('cust')),x.run(debug=True)] , [__import__('flask').Flask(__name__)])
+cmv = {'get': lambda self: "devuelve todo",'post' : lambda self: "crear",'put' : lambda self: "actualizar"}
+map(lambda x: [x.add_url_rule('/', 'index', lambda: 'hola'), x.add_url_rule('/cust',view_func=type('Cust',(MV,),{row: eval("cmv['%s' % row]") for row in ['get','post','put']}).as_view('cust')),x.run(debug=True)] , [__import__('flask').Flask(__name__)])
